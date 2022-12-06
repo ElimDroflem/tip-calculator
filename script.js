ca009b2bae640651ecf448378949bf03ad12442d
ElemDroflem <105154979+ElimDroflem@users.noreply.github.com>
2022-12-06 23:01:28 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,63 +1,37 @@
 const bill = document.getElementById("bill");
+const buttons = document.getElementsByClassName("btn");
 const numberOfPeople = document.getElementById("numberOfPeople");
-const reset = document.getElementById("reset");
+const calculate = document.getElementById("reset");
 const tipAmount = document.getElementById("tip-amount");
 const totalAmount = document.getElementById("tip-calc-total");
-const buttons = document.getElementsByClassName("btn");
 
 let percent = 0;
 
-reset.addEventListener("click", () => {
-  let totalTip = (percent / 100) * bill.value;
-  tipAmount.textContent = totalTip / numberOfPeople.value;
-
-  
-  let total = bill.value + tipAmount.textContent;
-  
-
-    //dahd
-  //   let total = bill.value + tipAmount.textContent;
-  //   totalAmount.textContent = total / numberOfPeople.value;
-  let addit = +bill.value + totalTip;
-  console.log("Conors Maths: " + addit);
-  totalAmount.textContent = addit;
-  console.log(totalTip);
+calculate.addEventListener("click", () => {
+  if (numberOfPeople.value != 0) {
+    numberOfPeople.style.color = "hsl(186, 14%, 43%)";
+    let totalTip = (percent / 100) * bill.value;
+    tipAmount.textContent = totalTip / numberOfPeople.value;
+    let addit = +bill.value + totalTip / numberOfPeople.value;
+    totalAmount.textContent = addit;
+  } else {
+    numberOfPeople.style.color = "red";
+  }
 });
 
 percentSelector = () => {
   for (const button of buttons) {
     button.addEventListener("click", () => {
-      percent += +button.textContent;
-      
-      
-      //console.log(percent);
+      if (percent === 0) {
+        percent += +button.textContent;
+        console.log(percent);
+      } else {
+        percent = 0;
+        percent += +button.textContent;
+        console.log(percent);
+      }
     });
   }
 };
 
 percentSelector();
-
-// bill +
-
-// 1) SAVE BILL VALUE
-// 2) ADD IN AMOUNT FOR TIP %
-// 3) SAVE NUMBER OF PEOPLE
-// 4) % / 100 * total
-// 5) tip amount = tip / no people
-// 6) total = bill amount + tip / no of people
-
-// collect value of the bill
-// find out what % they want to add on
-// find out how many people its split between
-
-// create a function that will store a value
-// 0 parameter
-// return value ( multiplier percentage) based on button selected
-
-// for (const button of buttons) {
-//     button.addEventListener("click", () => {
-//       console.log(button);
-//     });
-//   }
-
-// console.log((buttonPercent / 100) * totalValue);
